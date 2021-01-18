dbf4c72aace302c37e9368f5737ac52b2ee06fb4
tgilonis <tgilonis@protonmail.com>
2021-01-18 09:18:40 +0000

    Added to Sorting

---
+++ hackerrank-problems/src/main/java/Testing.java
@@ -0,0 +1,61 @@
+import org.w3c.dom.ls.LSOutput;
+
+import java.net.Inet4Address;
+import java.util.*;
+
+public class Testing
+{
+    static int hourglassSum(int[][] arr)
+    {
+        return 0;
+    }
+    static int[] rotLeft(int[] a, int d)
+    {
+        return null;
+    }
+
+    static int minimumSwaps(int[] arr)
+    {
+        return 0;
+    }
+
+    static long arrayManipulation(int n, int[][] queries)
+    {
+        return 0;
+    }
+
+    static void checkMagazine(String[] magazine, String[] note)
+    {
+
+    }
+    static String twoStrings(String s1, String s2)
+    {
+        return null;
+    }
+
+    static int sherlockAndAnagrams(String s)
+    {
+        return 0;
+    }
+
+    static long countTriplets(List<Long> arr, long r)
+    {
+        return 0;
+    }
+
+
+    public static void main(String[] args)
+    {
+
+        //System.out.println(Arrays.toString(rotLeft(new int[] {1,2,3,4,5}, 4)));
+
+        //System.out.println(minimumSwaps(new int[] {1,3,5,2,4,6,7}));
+
+        //System.out.println(sherlockAndAnagrams("ifailuhkqq"));
+        //System.out.println(sherlockAndAnagrams("kkkk"));
+
+        System.out.println(countTriplets(Arrays.asList(1L,2L,1L,2L,4L), 2));
+
+
+    }
+}
